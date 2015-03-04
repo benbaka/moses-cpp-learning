4d048215a581de8d443c5699b462352fc55a5b5a
moses olawoyin <m.bastford@gmail.com>
2015-03-04 21:41:38 +0000

    Created app: hello.closes #1

---
+++ hello/main.cc
@@ -0,0 +1,14 @@
+/*
+ *Program- Program that prints hello world
+ *
+ * Created by : Moses Olawoyin <m.bastford@gmail.com>
+ * Date : 04 March 2015
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( int argc, char *argv[])
+{
+    cout << "Hello world" << endl;
+}
