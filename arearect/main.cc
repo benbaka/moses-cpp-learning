1aaf89a9050ff6cf22b26867b85c7ee40ed896db
moses olawoyin <m.bastford@gmail.com>
2015-03-05 20:42:34 +0000

    creat app arearect. closes #4

---
+++ arearect/main.cc
@@ -0,0 +1,29 @@
+/*
+ * Area - Program that ask the user to enter the width and height of a rectangle
+ *        and then calculate the area of the rectangle.
+ *
+ *  Author - Moses Olawoyin <m.bastford@gmail.com>
+ *  Date - 05 March 2015.
+ */
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+    int height;
+    int width;
+    int area;
+
+    cout << "Enter the height of the rectangle : " << endl;
+    cin >> height;
+
+    cout << "Enter the width of the rectangle : " << endl;
+    cin >> width;
+
+    area = width * height;
+
+    cout << " The Area of the rectangle is :" << area << endl;
+
+    return 0;
+
+}
