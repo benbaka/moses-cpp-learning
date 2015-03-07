818d50a089f008bafe7c27a07566a040445db974
moses olawoyin <m.bastford@gmail.com>
2015-03-07 09:09:51 +0000

    intercept calculated.closes #17

--- straight-line/main.cc
+++ straight-line/main.cc
@@ -8,21 +8,24 @@ int main(int argc, char *argv[])
     int x2;
     int y2;
     int m;
+    int b;
 
-    cout << "Enter x1"<< endl;
+    cout << "Enter x1: ";
     cin >> x1;
 
-    cout << "Enter y1" << endl;
+    cout << "Enter y1: " ;
     cin >>y1;
 
-    cout << "Enter x2 " << endl;
+    cout << "Enter x2: ";
     cin >> x2;
 
-    cout <<"Enter y2" << endl;
+    cout <<"Enter y2: " ;
     cin >> y2;
 
     m =(y2-y1)/(x2-x1);
+    b = y1 - m*x1;
 
     cout << "The gradient of the line is :" << m << endl;
-    cout << "The equation of the line is :" << "y =" << m << * "x+b" << endl;
+    cout << "The intercept of the line is : " << b << endl;
+    cout << "The equation of the line is :" << "y =" << m << * "x"<< "+" << b << endl;
 }
