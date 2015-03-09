e87ff98fe5db991f7d214fba59aa0e40afe17317
moses olawoyin <m.bastford@gmail.com>
2015-03-09 22:12:04 +0000

    conditions added. closes #30

--- straight-line/main.cc
+++ straight-line/main.cc
@@ -22,8 +22,18 @@ int main(int argc, char *argv[])
     cout <<"Enter y2: " ;
     cin >> y2;
 
-    m =(y2-y1)/(x2-x1);
-    b = y1 - m*x1;
+      m =(y2-y1)/(x2-x1);
+      b = y1 - m*x1;
+
+    if(m == 0)
+    {
+        std::cout << "y = " << b << std::endl;
+    }
+    else
+    {
+        std::cout << "y = " << m << "x + " << b << std::endl;
+
+    }
 
     cout << "The gradient of the line is :" << m << endl;
     cout << "The intercept of the line is : " << b << endl;
