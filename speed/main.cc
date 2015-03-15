e9b51e5323bbb16e932e0c70ca43008906312d48
moses olawoyin <m.bastford@gmail.com>
2015-03-15 19:17:37 +0000

    created function read double. closes #61

--- speed/main.cc
+++ speed/main.cc
@@ -2,27 +2,40 @@
 
 inline double speed(double distance, double time)
 {
-    return distance * time;
+    return (distance/time);
+}
+
+double read_double(std::string prompt )
+{
+    double distance;
+
+    std::cout << prompt << " : ";
+    std::cin >> distance;
+
+    return distance;
+}
+
+double read_double_time(std::string prompt)
+{
+    double time;
+    std::cout << prompt << ":";
+    std::cin >> time;
+
+    return time;
 }
 
 int main (int argc, char* argv[])
 {
-    double distance;
-    double time;
-    double average_speed;
-
     
+    double average_speed;
+    double time;
+    double distance;
 
-
-    std::cout << "Enter the distance in meters: ";
-    std::cin >> distance;
-
-    std::cout << "Enter the time of movement in seconds: ";
-    std::cin >> time;
-
+    distance = read_double("Enter distance in meters");
+    time = read_double("Enter time in seconds");
     average_speed = speed(distance,time);
     
-
+    
     std::cout << "The average speed of the object is: " << average_speed << "m/s" <<std::endl;
     
 
