e43a243a663d3e160d683c096c9d91840550160e
moses olawoyin <m.bastford@gmail.com>
2015-03-19 00:07:15 +0000

    created function read in. closes

--- circle-area/main.cc
+++ circle-area/main.cc
@@ -8,8 +8,20 @@
  */
 
 #include <iostream>
+
+double read_double(std::string prompt)
+{
+    double value;
+
+    std::cout << prompt << ":";
+    std::cin >> value;
+
+    return value;
+}
+
 double circle_area( double r)
 {
+ 
     const double PI = 3.142;
 
     return r*r*PI;
@@ -21,12 +33,10 @@ int main(int argc, char *argv[])
 
     double r;
     double area;
-    const double PI = 3.142;
+    double radius;
 
-    std::cout << "Enter the radius of the circle: ";
-    std::cin >> r;
-
-    area = circle_area(r);
+     radius = read_double("Enter radius");
+     area = circle_area(r);
 
     std::cout << "The area of the circle is: " << area << std::endl;
 
