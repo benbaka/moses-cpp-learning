8a2e2db62af7ad9d129be63151ecc1de261a69e4
moses olawoyin <m.bastford@gmail.com>
2015-03-20 21:41:48 +0000

    Fixed name of constructor. closes #75

--- point/main.cc
+++ point/main.cc
@@ -5,56 +5,11 @@ class Point
 {
     public:
 
-        point();
+        Point();
 
-        double  x();
-        void set_x(double x);
-
-        double y();
-        void set_y(double y);
-
-    private:
-        double m_x;
-        double m_y;
+        
 
 
 };
-int Point::get_x()
-{
-    return m_x;
-}
-int Point::get_y()
-{
-    return m_x;
-}
-int Point::Point()
-{
-    return
-}
-int Point:: Point(double,double)
-{
-    return 0;
-}
-int set_x(double, double)
-{
-    return m_x;
-}
-int set_y(double, double)
-{
-    return m_y;
-}
-
-
-int main (int argc, char* argv[])
-{
-    Point p;
-    p.set_x(4,3);
-    p.set_y(2,4);
-
-    std::cout << p.get_x() <<std::endl;
-    std::cout << p.get_y() << std::endl;
-
-    return 0;
-}
 
 
