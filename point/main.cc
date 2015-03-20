227292e13e36c48deee0c3bacea2143edfc66459
moses olawoyin <m.bastford@gmail.com>
2015-03-20 23:01:57 +0000

    Created accessor name to match declaration.closes #79

--- point/main.cc
+++ point/main.cc
@@ -6,10 +6,34 @@ class Point
     public:
 
         Point();
+        Point(double , double);
+        double x();
+        void set_x(double);
+        double y();
+        void set_y(double);
 
-        
-
+    private:
+        double m_x;
+        double m_y;
 
 };
+
+void Point::set_x(double x)
+{
+    return m_x;
+}
+void Point::set_y(double y)
+{
+    return m_y;
+
+}
+Point::Point()
+{
+    set_x(0);
+    set_y(0);
+
+}
+
+
 
 
