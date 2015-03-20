2f206b3097c048ad45251eb2c19b9b7bf7f2a489
moses olawoyin <m.bastford@gmail.com>
2015-03-20 23:18:47 +0000

    created Point and set_x and set_y. closes #76,#72

--- point/main.cc
+++ point/main.cc
@@ -20,17 +20,23 @@ class Point
 
 void Point::set_x(double x)
 {
-    return m_x;
+     m_x = x;
 }
 void Point::set_y(double y)
 {
-    return m_y;
+    m_y = y;
 
 }
 Point::Point()
 {
     set_x(0);
     set_y(0);
+
+}
+Point::Point( double x, double y)
+{
+    set_x(x);
+    set_y(y);
 
 }
 
