e505bb6f746b65c986ce13736601cc6c89fb1fae
moses olawoyin <m.bastford@gmail.com>
2015-03-21 01:21:00 +0000

    created point. closes

--- point/main.cc
+++ point/main.cc
@@ -22,22 +22,35 @@ void Point::set_x(double x)
 {
      m_x = x;
 }
+
 void Point::set_y(double y)
 {
     m_y = y;
 
 }
+
 Point::Point()
 {
     set_x(0);
     set_y(0);
 
 }
+
 Point::Point( double x, double y)
 {
     set_x(x);
     set_y(y);
 
+}
+
+int main (int argc, char* argv[])
+{
+    Point p1;
+    p1.set_x(4);
+    p1.set_y(6);
+
+    std::cout << " Point x is: " << p1.set_x <<std::endl;
+    std::cout << " Point y is: " << p1.set_y << std::endl;
 }
 
 
