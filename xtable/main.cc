0330ee29fcce36a5365a4c8f4c1c05d1f5258233
moses olawoyin <m.bastford@gmail.com>
2015-03-24 22:24:41 +0000

    Removed magic number 12. closes #89

--- xtable/main.cc
+++ xtable/main.cc
@@ -13,31 +13,32 @@
 
 int main(int argc, char *argv[])
 {
+  int const size = 12;
     std::cout << "     |";
-    for (int x = 1; x<=12; ++x)
+    for (int x = 1; x<=size; ++x)
     {
         std:: cout <<std::setw(5)<< x <<"|";
         
     }
     std:: cout <<std::endl;
 
-    for(int x = 0; x<=12; ++x)
+    for(int x = 0; x<=size; ++x)
     {
         std::cout << "-----+";
     }
     std::cout << std::endl;
 
-    for(int y = 1; y<=12; ++y)
+    for(int y = 1; y<=size; ++y)
     {
         std::cout<<std::setw(5)<< y << "|";
 
-        for (int x=1; x<=12; ++x)
+        for (int x=1; x<=size; ++x)
         {
             std::cout<<std::setw(5)<<x*y << "|";
         }
         std::cout<< std::endl;
 
-        for(int x =0; x<=12; ++x)
+        for(int x =0; x<=size; ++x)
         {
             std::cout << "-----+";
         }
